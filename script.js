bc1d0fc935c59afaf48b3d91c0984a55bd3327f7
Khaair <khair.cse.iu@gmail.com>
2022-01-17 22:00:37 +0600

    Update

--- script.js
+++ script.js
@@ -35,17 +35,17 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web Developer", " Web Designer", "Freelancer", "Blogger", "YouTuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web Developer", " Web Designer", "Freelancer", "Blogger", "YouTuber"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true  
     });
 
     // owl carousel script
